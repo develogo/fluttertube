0fe1c68d7d3eea1d661eed7ee1dc8bca41e4b425
Felipe Andrade <lippeandrade@live.com>
2019-11-12 08:45:01 -0300

    Search page

--- lib/delegate/data_search.dart
+++ lib/delegate/data_search.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -34,14 +36,48 @@ class DataSearch extends SearchDelegate<String>{
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    if(query.isEmpty)
+      return Container();
+    else
+      return FutureBuilder<List>(
+        future: suggestions(query),
+        builder: (context, snapshot){
+          if(!snapshot.hasData){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+                itemBuilder: (context, index){
+                  return ListTile(
+                    title: Text(snapshot.data[index]),
+                    leading: Icon(Icons.play_arrow),
+                    onTap: (){
+                      close(context, snapshot.data[index]);
+                    },
+                  );
+                },
+                itemCount: snapshot.data.length,
+            );
+          }
+        },
+      );
   }
 
-  suggestions(String search) async {
+  Future<List> suggestions(String search) async {
 
     http.Response response = await http.get(
-
+      "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
     );
+
+    if(response.statusCode == 200){
+      return json.decode(response.body)[1].map((v){
+        return v[0];
+      }).toList();
+
+    } else {
+      throw Exception("Failed to load suggestions");
+    }
 
   }
 
